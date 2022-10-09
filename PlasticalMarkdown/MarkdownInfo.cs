6fa6de336c16db07bba94aca66ed13386ceed5a6
Katy248 <petrovanton247@gmail.com>
2022-10-09 21:13:46 +0300

    Добавлен конструктор

--- PlasticalMarkdown/MarkdownInfo.cs
+++ PlasticalMarkdown/MarkdownInfo.cs
@@ -8,6 +8,32 @@ using System.Threading.Tasks;
 namespace PlasticalMarkdown;
 public class MarkdownInfo
 {
+     #region Constructors
+
+    public MarkdownInfo(string path)
+    {
+        File = new FileInfo(path);
+    }
+    public MarkdownInfo(FileInfo file)
+    {
+        File = file;
+    }
+
+    #endregion
+
+    public FileInfo File 
+    {
+        get => file ?? new FileInfo("");
+        private set
+        {
+            file = value;
+            if (!file.Exists)
+                throw new FileNotFoundException($"File \"{file.FullName}\" doesn't exist.");
+        }
+    }
+
+    private FileInfo? file;
+
     public IEnumerable<MarkdownItem> Parse(IMarkdownParser parser)
     {
         return parser.Parse(this);
