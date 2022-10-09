9117a144f36857cc754f0fa5820b04995154946a
Katy248 <petrovanton247@gmail.com>
2022-10-09 14:33:24 +0300

    Добавлен метод Parse

--- PlasticalMarkdown/MarkdownInfo.cs
+++ PlasticalMarkdown/MarkdownInfo.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PlasticalMarkdown;
 public class MarkdownInfo
 {
+    public IEnumerable<MarkdownItem> Parse(IMarkdownParser parser)
+    {
+        return parser.Parse(this);
+    }
 }
