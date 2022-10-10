35795af6eb52596096c98a8ae0ec85869692cea0
Katy248 <petrovanton247@gmail.com>
2022-10-10 09:32:29 +0300

    Добавлены производные класса SuikaItem

--- PlasticalMarkdown/SuikaNotation/SuikaItem.cs
+++ PlasticalMarkdown/SuikaNotation/SuikaItem.cs
@@ -13,6 +13,35 @@ public class SuikaItem : MarkdownItem
     }
     public ItemType Type { get; set; }
 }
+public class FunctionItem : SuikaItem
+{
+    public FunctionItem(string value, ItemType type) : base(value, type)
+    {
+
+    }
+}
+public class TextItem : SuikaItem
+{
+    public TextItem(string speech, string speaker, string value, ItemType type) : base(value, type)
+    {
+        Speech = speech;
+        Speaker = speaker;
+    }
+    public string Speech { get; private set; }
+    private string? speaker;
+    public string Speaker 
+    {
+        get => speaker ?? ""; 
+        private set => speaker = value; 
+    }
+}
+public class LabelItem : SuikaItem
+{
+    public LabelItem(string value, ItemType type) : base(value, type)
+    {
+
+    }
+}
 public enum ItemType
 {
     TextOutput,
