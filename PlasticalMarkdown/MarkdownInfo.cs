fdec11a8f51819499d06672f0b68f3f11d91e412
Katy248 <petrovanton247@gmail.com>
2022-10-10 09:33:14 +0300

    Добавлен забытый using

--- PlasticalMarkdown/MarkdownInfo.cs
+++ PlasticalMarkdown/MarkdownInfo.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Emit;
-using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace PlasticalMarkdown;
