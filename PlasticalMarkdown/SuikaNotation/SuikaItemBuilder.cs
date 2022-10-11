7fde070d87c0cdf1888f189446c8fd00b1bc9f46
Katy248 <petrovanton247@gmail.com>
2022-10-11 08:41:33 +0300

    Изменена последовательность создания объекта

--- PlasticalMarkdown/SuikaNotation/SuikaItemBuilder.cs
+++ PlasticalMarkdown/SuikaNotation/SuikaItemBuilder.cs
@@ -10,28 +10,35 @@ public class SuikaItemBuilder
 {
     public SuikaItemBuilder(string value, ItemType type)
     {
+        this.value = value;
+        this.type = type;
+    }
+
+    private readonly string value;
+    private readonly ItemType type;
+    
+    private SuikaItem? item;
+
+    public SuikaItem ToSuikaItem()
+    {
         switch (type)
         {
             case ItemType.TextOutput:
                 Match match = Regex.Match(value, @"\*[\n]*\*");
                 if (match.Success)
-                    Item = new TextItem(value.Replace(match.Value, ""), match.Value, value);
+                    item = new TextItem(value.Replace(match.Value, ""), match.Value, value);
                 else
-                    Item = new TextItem(value, "", value);
+                    item = new TextItem(value, "", value);
                 break;
             case ItemType.Function:
-                Item = new FunctionItem(value);
+                item = new FunctionItem(value);
                 break;
             case ItemType.Label:
-                Item = new LabelItem(value);
+                item = new LabelItem(value);
                 break;
             default:
                 break;
         }
-    }
-    SuikaItem Item;
-    public SuikaItem ToSuikaItem()
-    {
-        return Item;
+        return item;
     }
 }
