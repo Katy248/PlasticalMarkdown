4b6ecf36726357c7830d453a01d501fb8037b841
Katy248 <petrovanton247@gmail.com>
2022-10-12 10:21:16 +0300

    Добавлены структуры для испольнения функций

---
+++ PlasticalMarkdown/SuikaNotation/Functions/SuikaFunctionExecuter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlasticalMarkdown.SuikaNotation.Functions;
+
+public class SuikaFunctionExecuter
+{
+    private List<Function> functions = new List<Function>();
+    public SuikaFunctionExecuter AddFunction(Function? function)
+    {
+        if (function is not null)
+            functions.Add(function);
+        return this;
+    }
+    public SuikaFunctionExecuter AddFunctions(IEnumerable<Function> functions)
+    {
+        foreach (var fn in functions)
+        {
+            AddFunction(fn);
+        }
+        return this;
+    }
+    public void ExecuteFunction(FunctionItem functionItem)
+    {
+        ExecuteFunction(functionItem.Name, functionItem.Args);
+    }
+    public void ExecuteFunction(string functionName, string[] arguments)
+    {
+        functions
+            .FirstOrDefault(_ => _.Name == functionName)
+            ?.FunctionHandler
+            .Invoke(arguments);
+    }
+
+}
