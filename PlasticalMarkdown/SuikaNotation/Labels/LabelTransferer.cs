a034ce2aa6061aa0accfa808f5197f0a8d76db17
Katy248 <petrovanton247@gmail.com>
2022-10-15 18:32:09 +0300

    Добавлена реализация

--- PlasticalMarkdown/SuikaNotation/Labels/LabelTransferer.cs
+++ PlasticalMarkdown/SuikaNotation/Labels/LabelTransferer.cs
@@ -5,6 +5,25 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace PlasticalMarkdown.SuikaNotation.Labels;
-internal class LabelTransferer
+public class LabelTransferer
 {
+    public LabelTransferer(ISuikaMarkdownParser suikaParser)
+    {
+        parser = suikaParser;
+        var lines = parser.Lines.ToArray();
+
+        //fill labels
+        for (int i = 0; i < lines.Length; i++)
+            if (lines[i].Trim().StartsWith(':'))
+                labels.Add(lines[i].Remove(0, 1), i);
+    }
+    private readonly ISuikaMarkdownParser parser;
+    private Dictionary<string, int> labels = new Dictionary<string, int>();
+    public void SetPosition(string label)
+    {
+        if (labels.TryGetValue(label, out int newPosition))
+        {
+            parser.CurrentLineIndex = newPosition;
+        }
+    }
 }
