ee9fc4c21048a236c3aaaf879907825ffed2fd62
Katy248 <petrovanton247@gmail.com>
2022-10-15 11:35:00 +0300

    Добавлены документация и метод пререхода к метке

--- PlasticalMarkdown/IMarkdownParser.cs
+++ PlasticalMarkdown/IMarkdownParser.cs
@@ -7,7 +7,24 @@ using System.Threading.Tasks;
 namespace PlasticalMarkdown;
 public interface IMarkdownParser
 {
+    /// <summary>
+    /// Parse all file.
+    /// </summary>
+    /// <returns><see cref="IEnumerable{MarkdownInfo}"/> of parsed <see cref="MarkdownItem"/>.</returns>
     IEnumerable<MarkdownItem> Parse();
+    /// <summary>
+    /// Parse line.
+    /// </summary>
+    /// <returns>Parsed <see cref="MarkdownItem"/>.</returns>
     MarkdownItem ParseLine();
+    /// <summary>
+    /// Parse next line.
+    /// </summary>
+    /// <returns>If next line exists then parsed <see cref="MarkdownItem"/> else <see cref="null"/>.</returns>
     MarkdownItem? ParseNext();
+    /// <summary>
+    /// Get current parsed <see cref="MarkdownItem"/>.
+    /// </summary>
+    MarkdownItem CurrentItem { get; }
+    void GoTo(string mark);
 }
