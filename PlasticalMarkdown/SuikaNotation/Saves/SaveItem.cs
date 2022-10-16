fce81132046ef0a7193e89248380cc66a3d92136
Katy248 <petrovanton247@gmail.com>
2022-10-16 20:31:08 +0300

    Добавлен абстрактный класс для сохранений

---
+++ PlasticalMarkdown/SuikaNotation/Saves/SaveItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlasticalMarkdown.SuikaNotation.Saves;
+public abstract class SaveItem
+{
+    public int LineIndex { get; set; }
+}
