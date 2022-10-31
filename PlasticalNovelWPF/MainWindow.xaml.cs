3ecc4b6875587cace9e5c5f5e00576f16c66fdb8
Katy248 <petrovanton247@gmail.com>
2022-10-31 09:03:13 +0300

    Добавлена логика взаимодействия

--- PlasticalNovelWPF/MainWindow.xaml.cs
+++ PlasticalNovelWPF/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using PlasticalMarkdown.SuikaNotation.Functions;
+using PlasticalMarkdown.SuikaNotation.Labels;
+using PlasticalMarkdown.SuikaNotation;
+using PlasticalMarkdown;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,5 +26,26 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        var mi = new MarkdownInfo("script.txt");
+        processor = new SuikaProcessor(mi);
+
+        processor.ParsedTextItem += ShowText;
+        processor.ScriptEnds += EndHandler;
+    }
+    private readonly SuikaProcessor processor;
+
+    void ShowText(TextItem textItem)
+    {
+        SpeakerBlock.Text = textItem.Speaker.Trim();
+        SpeechBlock.Text = textItem.Speech.Trim();
+    }
+    void EndHandler()
+    {
+        MessageBox.Show("This is the End!");
+    }
+
+    private void Canvas_MouseUp(object sender, MouseButtonEventArgs e)
+    {
+        processor.Next();
     }
 }
